0e6f0da5ec20a32c3a9ba93285a9bd7388178cf8
westee <904885508@qq.com>
2020-05-11 22:23:16 +0800

    将 Styled Component 改造为 Function Component

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React from "react";
 
-const TagsSection = styled.section`
+const _TagsSection = styled.section`
   background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -18,6 +19,19 @@ const TagsSection = styled.section`
   }
 `;
 
+const TagsSection: React.FC = () => {
+    return (
+        <_TagsSection>
+            <ol>
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ol>
+            <button>新增标签</button>
+        </_TagsSection>
+    )
+}
 export default TagsSection;
 
 

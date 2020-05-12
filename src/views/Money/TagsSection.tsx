0fd81b39403e6e54a16ca9e44e35dc397ef0093e
westee <904885508@qq.com>
2020-05-12 22:53:47 +0800

    完成标签的选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+import mouseUp = Simulate.mouseUp;
 
-const _TagsSection = styled.section`
+const TagWrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -10,6 +12,7 @@ const _TagsSection = styled.section`
        background: #D9D9D9; border-radius: 18px;
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
+       &.selected{background: red;}
     }
   }
   > button{
@@ -20,16 +23,35 @@ const _TagsSection = styled.section`
 `;
 
 const TagsSection: React.FC = () => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTag, setSelectedTag] = useState<string[]>([]);
+    const onAddTag = () => {
+        const tagName = window.prompt('请输入新的标签名');
+        if (tagName !== null) {
+            setTags([...tags, tagName])
+        }
+    };
+
+    const onToggleTag = (tag: string) => {
+        const index = selectedTag.indexOf(tag)
+        if(index >= 0){
+            setSelectedTag(selectedTag.filter(t => t !== tag));
+        } else {
+            setSelectedTag([...selectedTag, tag]);
+        }
+    }
+
     return (
-        <_TagsSection>
+        <TagWrapper>
             <ol>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
+                {tags.map(tag =>
+                    <li key={tag} onClick={() => {
+                        onToggleTag(tag)
+                    }} className={selectedTag.indexOf(tag) >= 0 ? 'selected' : ''}>{tag}</li>
+                )}
             </ol>
-            <button>新增标签</button>
-        </_TagsSection>
+            <button onClick={onAddTag}>新增标签</button>
+        </TagWrapper>
     )
 }
 export default TagsSection;

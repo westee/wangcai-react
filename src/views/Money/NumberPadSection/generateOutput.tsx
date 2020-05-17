f09b99f9a6ea0704110c7b0b79695a0caa7a907c
westee <904885508@qq.com>
2020-05-17 09:09:27 +0800

    优化数字面板

---
+++ src/views/Money/NumberPadSection/generateOutput.tsx
@@ -0,0 +1,37 @@
+const generateOutput = (text: string, output: string) => {
+    switch (text) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if (output === '0') {
+                return text;
+            } else {
+                return output + text;
+            }
+        case '.':
+            if (output.indexOf('.') >= 0) {
+                return ''
+            }
+            return output + '.';
+        case '删除':
+            if (output.length === 1) {
+               return '';
+            } else {
+                return output.slice(0, -1);
+            }
+        case 'OK':
+        case '清空':
+            return '';
+        default:
+            return '';
+    }
+};
+
+export default generateOutput;

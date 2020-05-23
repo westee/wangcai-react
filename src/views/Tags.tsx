6d2404148c6c2d42414d21abaee52b11763d99a3
westee <904885508@qq.com>
2020-05-23 21:47:48 +0800

    抽取useTags函数

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,7 +1,9 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useState} from "react";
+import useTags from "../useTags";
 
 function Tags() {
+    const {tags, setTags} = useTags();
     return  (
         <Layout>
             <h2>Tags</h2>

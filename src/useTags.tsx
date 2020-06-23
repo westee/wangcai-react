eab5e7a43d62d6a26ea8423e6eb367c98ec2c292
westee <904885508@qq.com>
2020-06-23 21:12:18 +0800

    功能：删除 tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -20,19 +20,12 @@ const useTags = () => {
         }
         return result;
     };
-    const updateTag = (id: number, obj: {name: string}) => {
-        const index = findTagIndex(id);
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        tagsClone.splice(index, 1, {id, name: obj.name});
-        setTags(tagsClone);
+    const updateTag = (id: number, {name}: {name: string}) => {
+        setTags(tags.map(tag => tag.id === id ? {id, name: name} :tag));重构：更优雅的更新与删除
 
     };
     const deleteTag = (id: number) => {
-        const index = findTagIndex(id);
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        tagsClone.splice(index, 1);
-        setTags(tagsClone);
-
+        setTags(tags.filter(tag => tag.id !== id))
     };
 
     return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};

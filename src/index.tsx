1fcfa2f873fd4fbc5f11bfa7ee76e1f606c99c0e
dflk <904885508@qq.com>
2023-01-06 22:02:20 +0800

    使用createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'index.scss';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
